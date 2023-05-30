099bc3effa08ae83b864898cafafb74918dc185d
victorcals <victorcals78@gmail.com>
2023-05-30 09:41:18 -0300

    Ajustes || alteração de porta para 3003

--- CrudNode/app.js
+++ CrudNode/app.js
@@ -11,7 +11,7 @@ var cadastroRouter = require('./routes/cadastroRouter')
 
 var app = express();
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
