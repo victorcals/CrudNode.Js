0f1394eeba735f8fb4eba1cd8fd88b0643a2e064
guilhermeandrade02 <guilhermeandrade02@hotmail.com>
2023-05-30 22:18:00 -0300

    pesquisa por dados

--- CrudNode/controllers/cadastroController.js
+++ CrudNode/controllers/cadastroController.js
@@ -15,6 +15,36 @@ class cadastroController {
         res.status(200).json(resultado);
     }
 
+    async buscarPorNome(req, res) {
+        const nome = req.params.nome;
+        const resultado = await cadastroModel.findOne({ 'nome': nome });
+        res.status(200).json(resultado);
+    }
+
+    async buscarPorSobrenome(req, res) {
+        const sobrenome = req.params.sobrenome;
+        const resultado = await cadastroModel.findOne({ 'sobrenome': sobrenome });
+        res.status(200).json(resultado);
+    }  
+    
+    async buscarPorCidade(req, res) {
+        const cidade = req.params.cidade;
+        const resultado = await cadastroModel.findOne({ 'cidade': cidade });
+        res.status(200).json(resultado);
+    }  
+
+    async buscarPorEstado(req, res) {
+        const estado = req.params.estado;
+        const resultado = await cadastroModel.findOne({ 'estado': estado });
+        res.status(200).json(resultado);
+    }  
+
+    async buscarPorStatus(req, res) {
+        const status = req.params.status;
+        const resultado = await cadastroModel.findOne({ 'status': status });
+        res.status(200).json(resultado);
+    } 
+
     async buscarPorId(req, res) {
         const id = req.params.id;
         const resultado = await cadastroModel.findOne({ 'id': id });
--- CrudNode/routes/cadastroRouter.js
+++ CrudNode/routes/cadastroRouter.js
@@ -8,6 +8,11 @@ const cadastroController = require('../controllers/cadastroController');
 router.get('/', cadastroController.listar)
 router.post('/', cadastroController.salvar)
 router.get('/:id', cadastroController.buscarPorId)
+router.get('/nome/:nome', cadastroController.buscarPorNome)
+router.get('/sobrenome/:sobrenome', cadastroController.buscarPorSobrenome)
+router.get('/cidade/:cidade', cadastroController.buscarPorCidade)
+router.get('/estado/:estado', cadastroController.buscarPorEstado)
+router.get('/status/:status', cadastroController.buscarPorStatus)
 router.put('/:id', cadastroController.atualizar)
 router.delete('/:id', cadastroController.excluir)
 
